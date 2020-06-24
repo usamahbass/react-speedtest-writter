e175b87a48c7e59ee29dd9379acf54de33cd77d3
usamahbass <basalamahusamah7@gmail.com>
2020-06-24 21:15:03 +0700

    adding Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,3 @@
-// import React, { useState, useContext } from "react";
-// import { Display } from "./components/Display";
-// import { Score } from "./components/Score";
-// import { Timer } from "./components/Timer";
-// import { Switch } from "./components/Switch";
-// import Text from "./config/Text";
-
-// function DisplayContent({ displayContent,  })
-
-// const App = () => {
-//   const [app, setApp] = useState({
-//     text: Text(),
-//     userType: "",
-//     timer: 0,
-//     started: false,
-//     finished: false
-//   });
-
-//   function handleText(e) {
-//     e.preventDefault();
-//     getTimer();
-//     getFinished(e.target.value);
-
-//     setApp({
-//       ...app,
-//       userType: e.target.value
-//     });
-//   }
-
-//   const getTimer = () => {
-//     if (!app.started) {
-//       setApp({
-//         ...app,
-//         started: true
-//       });
-//       setInterval(() => {
-//         setApp(paramsTimer => {
-//           return {
-//             ...app,
-//             timer: paramsTimer.timer + 1
-//           };
-//         });
-//       }, 1000);
-//     }
-//   };
-
-//   const getFinished = userType => {
-//     if (userType === app.text) {
-//       clearInterval();
-//       setApp({
-//         ...app,
-//         finished: true
-//       });
-//     }
-//   };
-
-//   const restartType = () => {
-//     setApp({
-//       ...app,
-//       setUserType: "",
-//       started: false,
-//       timer: 0
-//     });
-//   };
-
-//   const refreshText = () => {
-//     window.location.reload();
-//   };
-
-//   return (
-//     <div>
-//       <div className="container mb-5 mb-5">
-//         <div className="col-md-6 offset-md-3 mt-5">
-//           <h1 className="text-center">Learn State</h1>
-//           <Timer timer={app.timer} />
-//           <Switch
-//             click={toggleTheme}
-//             value={isLightTheme}
-//             checked={!isLightTheme}
-//           />
-//           <Display text={app.text} userType={app.userType} />
-//           <textarea
-//             value={app.userType}
-//             className="form-control mb-2"
-//             onChange={handleText}
-//           ></textarea>
-
-//           <div className="text-right mt-3">
-//             <button className="btn btn-secondary mr-3" onClick={restartType}>
-//               Ulangi
-//             </button>
-
-//             <button className="btn btn-secondary" onClick={refreshText}>
-//               Ganti Text
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { Component } from "react";
 
 import { Display } from "./components/Display";
@@ -120,52 +16,72 @@ export default class App extends Component {
       text: Text(),
       userType: "",
       timer: 0,
+      score: 0,
       started: false,
-      finished: false
+      finished: false,
     };
   }
 
   componentDidMount() {
+    const elem = document.querySelector("#starting");
+
+    if (elem) {
+      elem.remove();
+    }
+
     let toggle = document.getElementById("switch");
 
-    toggle.addEventListener("change", function() {
+    toggle.addEventListener("change", function () {
       document.body.classList.toggle("dark-mode");
     });
   }
 
-  handleText = e => {
+  handleText = (e) => {
     e.preventDefault();
 
     this.setTimer();
+    this.setScore();
     this.getFinished(e.target.value);
-    this.setState(
-      {
-        userType: e.target.value
-      },
-      () => console.log(this.state.text)
-    );
+    this.setState({
+      userType: e.target.value,
+    });
   };
 
   setTimer = () => {
     if (!this.state.started) {
       this.setState({
-        started: true
+        started: true,
       });
       this.interval = setInterval(() => {
-        this.setState(paramsTimer => {
+        this.setState((paramsTimer) => {
           return {
-            timer: paramsTimer.timer + 1
+            timer: paramsTimer.timer + 1,
           };
         });
       }, 1000);
     }
   };
 
-  getFinished = userType => {
+  setScore = () => {
+    if (!this.state.started) {
+      this.setState({
+        started: true,
+      });
+      this.interval = setInterval(() => {
+        this.setState((paramsTimer) => {
+          return {
+            score: paramsTimer.timer + 5,
+          };
+        });
+      }, 1000);
+    }
+  };
+
+  getFinished = (userType) => {
     if (userType === this.state.text) {
       clearInterval(this.interval);
       this.setState({
-        finished: true
+        finished: true,
       });
 
       let finish = document.getElementById("finish");
@@ -179,7 +95,7 @@ export default class App extends Component {
     this.setState({
       userType: "",
       started: false,
-      timer: 0
+      timer: 0,
     });
   };
 
@@ -192,7 +108,7 @@ export default class App extends Component {
         <div className="container mb-5 mb-5">
           <div className="col-md-6 offset-md-3 mt-5">
             <h1 className="text-center">RAKSYE WRITER</h1>
-            <Score score={this.state.timer} />
+            <Score score={this.state.score} />
             <Timer timer={this.state.timer} />
             <Switch
               click={this.clickSwitch}
